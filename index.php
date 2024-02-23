53a3b806d4f760493388550002120c0ceaf1192a
EleonoraFlaborea <flaboreaeleonora@gmail.com>
2024-02-23 15:26:59 +0100

    add construct

--- index.php
+++ index.php
@@ -9,13 +9,21 @@ create un file index.php in cui:
 */
 
 class Movie {
-    //variabili d'istanza
+    //instance variables
     public $name;
-    public $cast;
+    public $characters;
     public $year;
+    //construction
+    public function __construct($name, $characters, $year){
+        $this->name = $name;
+        $this->characters = $characters;
+        $this->year = $year;
+
+    }
 }
 
-$movie1 = new Movie();
+$movie1 = new Movie('Harry Potter e la pietra filosofale','Harry, Ron, Hermione...', 2001);
+
 var_dump($movie1)
 
 ?>
